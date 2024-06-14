eea5f560e1d77520cd1d984d485393f063e03a4b
andreagonzalezsilva <andreagonzalezs1708@gmail.com>
2024-06-13 20:06:08 -0700

    Add the terraform configuration

--- compute.tf
+++ compute.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "web1" {
   security_groups = [aws_security_group.web_sg.id]
   key_name      = aws_key_pair.generated_key.key_name
   tags = {
-    Name = "CCTB-ProductionEnv"
+    Name = "CCTB-ProductionEnv1"
   }
 }
 
@@ -17,6 +17,41 @@ resource "aws_instance" "web2" {
   security_groups = [aws_security_group.web_sg.id]
   key_name      = aws_key_pair.generated_key.key_name
   tags = {
-    Name = "CCTB-ProductionEnv"
+    Name = "CCTB-ProductionEnv2"
   }
+
+
+resource "aws_instance" "web3" {
+  ami           = "ami-0440d3b780d96b29d" # Amazon Linux 2 AMI (HVM)
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.main_b.id
+  security_groups = [aws_security_group.web_sg.id]
+  key_name      = aws_key_pair.generated_key.key_name
+  tags = {
+    Name = "Testing_Env"
+  }
+
+resource "aws_instance" "web4" {
+  ami           = "ami-0440d3b780d96b29d" # Amazon Linux 2 AMI (HVM)
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.main_b.id
+  security_groups = [aws_security_group.web_sg.id]
+  key_name      = aws_key_pair.generated_key.key_name
+  tags = {
+    Name = "Staging_Env"
+  }
+
+resource "aws_instance" "web5" {
+  ami           = "ami-0440d3b780d96b29d" # Amazon Linux 2 AMI (HVM)
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.main_b.id
+  security_groups = [aws_security_group.web_sg.id]
+  key_name      = aws_key_pair.generated_key.key_name
+  user_data     = file("jenkins_install.sh")
+  tags = {
+    Name = "JenkinsController"
+  }
+
+
+  
 }
