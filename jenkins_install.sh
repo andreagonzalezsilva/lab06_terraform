b351cf5b997478af92cb39e02f6e1f889215b75c
andreagonzalezsilva <andreagonzalezs1708@gmail.com>
2024-06-13 21:07:48 -0700

    Modify jenkins install

--- jenkins_install.sh
+++ jenkins_install.sh
@@ -5,8 +5,8 @@ sudo yum -y install npm
 sudo yum -y update
 sudo yum -y install java-21-amazon-corretto
 sudo yum -y install git
-sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repoLinks to an external site.
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.keyLinks to an external site.
+sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repoLinks
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.keyLinks
 sudo yum -y install jenkins
 sudo fallocate -l 1G /swapfile_extend_1GB
 sudo mount -o remount,size=5G /tmp/
